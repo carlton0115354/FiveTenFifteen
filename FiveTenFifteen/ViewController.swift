0ec44758911075c8ddd61eee088e5a83d4ae48f8
Carlton <b0209101@gmail.com>
2022-04-05 22:43:15 +0800

    Initial Commit

---
+++ FiveTenFifteen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FiveTenFifteen
+//
+//  Created by 朱家成 on 2022/4/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
